0b756de9d699db079245e715c2197a053e399d8d
Liu Qiang <liuqiang44@live.cn>
2022-07-17 00:24:24 +0800

    练习项目-随机生成序列

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li
+        v-for="(num,index) in nums"
+        :key="index"
+      >
+      {{num}}
+      {{index}}
+      <button @click="remove(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="add()">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // },
+  data(){
+    return {
+      nums:[Math.floor(Math.random()*100),Math.floor(Math.random()*100)]
+    }
+  },
+  methods:{
+    remove(index) {
+      console.log(index)
+        this.nums.splice(index,1)
+  },
+  add(){
+    let newnum=Math.floor(Math.random()*100)
+    console.log(newnum)
+    this.nums.push(newnum)
+  }
+}
+}
+</script>
+
+<style scoped>
+
+</style>
